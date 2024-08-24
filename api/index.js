cb47358e1f762e72e5e146157f598359ddc7af2d
Rahul Meher <57453928+rahulmeher23@users.noreply.github.com>
2024-08-24 12:57:21 +0530

    refactor: moved index.js to /api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const router = require('./routes/routes')
+const router = require('../routes/routes')
 const express = require("express");
 const multer = require("multer");
 const bodyParser = require("body-parser");
